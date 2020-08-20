da67cd8d3ee9ed92d74df3ff3fa4aa59b28c4acf
Javaughn Grant <javaughngrant@gmail.com>
2020-08-20 16:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     lineNamesNums.push((i+1) + ". "+ line[i])
   }
   if (line.length > 0) {
-    return `The line is currently: ${lineNamesNums.join(', '))}`
+    return `The line is currently: ${lineNamesNums.join(', ')}`
   } else {
     return `The line is currently empty.`
   }
